d22d97a12d8fbf4a8c255a7f9e9d13f8275187aa
rubikon02 <rubikon02@gmail.com>
2021-11-04 01:02:05 +0100

    Added vec2

--- src/game/components/utils/index.ts
+++ src/game/components/utils/index.ts
@@ -4,6 +4,7 @@ export {
 } from './utils'
 
 export { m4 } from './m4'
+export { Vec2 } from './Vec2'
 export { Vec3 } from './Vec3'
 export { log } from './log'
 export { Transform } from './Transform'
