4814ab2c3d20eeca9c0b59f88b2d07a077bab105
rubikon02 <rubikon02@gmail.com>
2021-11-06 22:20:01 +0100

    Simplified log

--- src/game/components/utils/log.ts
+++ src/game/components/utils/log.ts
@@ -1,7 +1,6 @@
 import { Vec3 } from './Vec3'
 
-export function log(name: string, value: Vec3 | string | number | boolean) {
-    if (typeof value === 'undefined') return
+export function log(name: string, value: any) {
     let element = document.getElementById(name)
     if (!element) {
         const ui = document.getElementById('ui')
@@ -21,22 +20,17 @@ export function log(name: string, value: Vec3 | string | number | boolean) {
         element = value
     }
 
-    if (value === null) {
-        value = 'null'
-    } else if (typeof value == 'boolean') {
-        value = value ? 'true' : 'false'
-    } else {
-        if (value instanceof Vec3) {
-            value = `x: ${value.x.toFixed(2)} y: ${value.y.toFixed(2)} z: ${value.z.toFixed(2)}`
-            element.parentElement.style.minWidth = '200px'
-        } else {
-            if (typeof value != 'string') {
-                value = String(value.toFixed(2))
-            } else {
-                value = JSON.stringify(value)
-            }
-        }
+    if (typeof value == 'undefined') {
+        value = 'undefined'
+    } else if (value instanceof Vec3) {
+        value = `x: ${value.x.toFixed(2)} y: ${value.y.toFixed(2)} z: ${value.z.toFixed(2)}`
+        element.parentElement.style.minWidth = '200px'
+    } else if (typeof value == 'number') {
+        value = value.toFixed(2).toString()
+    } else if (typeof value != 'string') {
+        value = JSON.stringify(value)
     }
+
     if (element.innerText != value) {
         element.innerText = value
     }
